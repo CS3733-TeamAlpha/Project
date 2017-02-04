7ea0e7d9a29fbfcb4db105875d64feccc76a908b
Sourec <crmyers@wpi.edu>
2017-02-04 16:11:15 -0500

    Implement Node/ConcreteNode... now featuring setters for x/y

--- src/main/java/pathfinding/ConcreteNode.java
+++ src/main/java/pathfinding/ConcreteNode.java
@@ -7,49 +7,70 @@ public class ConcreteNode implements Node
 {
 	private ArrayList<String> data;
 	private ArrayList<Node> neighbors;
-	private float x;
-	private float y;
+	private double x;
+	private double y;
 
-	public ConcreteNode(){}
+	public ConcreteNode()
+	{
+		data = new ArrayList<String>();
+		neighbors = new ArrayList<Node>();
+	}
 
 	public boolean containsData(String data)
 	{
-		return false;
+		return this.data.contains(data);
 	}
 
 	public void addData(Collection<String> newData)
 	{
-
+		data.addAll(newData);
 	}
 
-	public float distance(Node node)
+	public double distance(Node node)
 	{
-		return 0;
+		//TODO: Writes units tests... just because.
+		return Math.sqrt(Math.pow(x - node.getX(), 2) + Math.pow(y - node.getY(), 2));
 	}
 
-	public float distance(float nodeX, float nodeY)
+	public double distance(double nodeX, double nodeY)
 	{
-		return 0;
+		//TODO: Write unit tests
+		return Math.sqrt(Math.pow(x - nodeX, 2) + Math.pow(y - nodeY, 2));
 	}
 
 
 	public Collection<Node> getNeighbors()
 	{
-		return null;
+		return neighbors;
 	}
 
 	public void addNeighbors(Collection<Node> newNeighbors)
 	{
-
+		neighbors.addAll(newNeighbors);
 	}
 
-	public float getX()
+	public void addNeighbor(Node newNeighbor)
 	{
-		return 0;
+		neighbors.add(newNeighbor);
 	}
 
-	public float getY()
+	public double getX()
 	{
-		return 0;
+		return x;
+	}
+
+	public double getY()
+	{
+		return y;
+	}
+
+	public void setX(double newX)
+	{
+		x = newX;
+	}
+
+	public void setY(double newY)
+	{
+		y = newY;
 	}
 }
--- src/main/java/pathfinding/Node.java
+++ src/main/java/pathfinding/Node.java
@@ -6,11 +6,14 @@ public interface Node
 {
 	boolean containsData(String data);
 	void addData(Collection<String> newData);
-	float distance(Node node);
-	float distance(float nodeX, float nodeY);
+	double distance(Node node);
+	double distance(double nodeX, double nodeY);
 
 	Collection<Node> getNeighbors();
 	void addNeighbors(Collection<Node> newNeighbors);
-	float getX();
-	float getY();
+	void addNeighbor(Node newNeighbor);
+	double getX();
+	double getY();
+	void setX(double newX);
+	void setY(double newY);
 }
