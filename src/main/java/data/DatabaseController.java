a0225116d5e51315a5f4dac06df36c5b070a8752
solashirai <solashakashirai@gmail.com>
2017-02-05 16:22:02 -0500

    Fix bugs and add floor table

--- src/main/java/data/DatabaseController.java
+++ src/main/java/data/DatabaseController.java
@@ -12,6 +12,7 @@ public class DatabaseController {
 	private static String locationTable = "Location";
 	private static String officeTable = "Office";
 	private static String neighborTable = "Neighbor";
+	private static String floorTable = "Floor";
 	// jdbc Connection
 	private static Connection connection = null;
 	private static Statement stmt = null;
@@ -26,11 +27,13 @@ public class DatabaseController {
 
 		//initialize tables
 		initializeProviderTable();
+		initializeFloorTable();
 		initializeLocationTable();
 		initializeOfficeTable();
 		initializeNeighborTable();
 
 		//test insertions
+		insertFloor(1, "testbuilding", 1);
 		insertLocation(11, "testloc", "Room", 0, 0, 1);
 		insertLocation(12, "anotherloc", "Room", 1, 1, 1);
 		insertProvider(01, "John", "Doe");
@@ -47,6 +50,14 @@ public class DatabaseController {
 		getProviderLocations(01);
 		getProvidersAtLocation(11);
 		getProvidersAtLocation(12); //shouldn't have anything
+		getFloorInfo(1);
+
+		modifyFloor(1, "changef1", 2);
+		getFloorInfo(1);
+		modifyLocation(11, "asdf", "rm", 0, 0, 1);
+		getLocationByID(11);
+		modifyProvider(01, "sola", "shirai");
+		getProviderByID(01);
 
 		shutdown();
 	}
@@ -97,7 +108,17 @@ public class DatabaseController {
 			// No need to report an error.
 			// The table simply did not exist.
 		}
-	}
+		try
+		{
+			stmt = connection.createStatement();
+			// Drop the UnpaidOrder table.
+			stmt.execute("DROP TABLE Floor");
+			System.out.println("Floor table dropped.");
+		} catch (SQLException ex)
+		{
+			// No need to report an error.
+			// The table simply did not exist.
+		}	}
 
 	//examples of commands to create tables and attributes, etc.
 	//public static final String CREATE_ITEMS_DB = "CREATE TABLE items (item_id INTEGER NOT NULL, item_name VARCHAR(20) NOT NULL, item_price REAL NOT NULL, multiplicity_shop INTEGER NOT NULL, multiplicity_store INTEGER NOT NULL)";
@@ -164,7 +185,6 @@ public class DatabaseController {
 		try
 		{
 			//TODO: Location type is ok as a string? or change?
-			//TODO: FLoorID reference foreign key
 			stmt = connection.createStatement();
 			stmt.execute("CREATE TABLE Location(" +
 					"LocationID INT NOT NULL PRIMARY KEY, " +
@@ -172,7 +192,7 @@ public class DatabaseController {
 					"LocationType VARCHAR(10), " +
 					"XCoord INT, " +
 					"YCoord INT, " +
-					"FloorID INT" +
+					"FloorID INT REFERENCES Floor(FloorID)" +
 					")");
 			System.out.println("Location table initialized");
 			stmt.close();
@@ -208,6 +228,23 @@ public class DatabaseController {
 					"ToID INT REFERENCES Location(LocationID) " +
 					")");
 			System.out.println("Neighbor table initialized");
+			stmt.close();
+		}
+		catch (SQLException e){
+			e.printStackTrace();
+		}
+	}
+
+	public static void initializeFloorTable(){
+		try
+		{
+			stmt = connection.createStatement();
+			stmt.execute("CREATE TABLE Floor(" +
+					"FloorID INT NOT NULL PRIMARY KEY, " +
+					"Building VARCHAR(40), " +
+					"FloorLevel INT NOT NULL" +
+					")");
+			System.out.println("Floor table initialized");
 			stmt.close();
 		}
 		catch (SQLException e){
@@ -354,6 +391,29 @@ public class DatabaseController {
 		}
 	}
 
+	public static void getFloorInfo(int id){
+		try
+		{
+			stmt = connection.createStatement();
+
+			ResultSet results = stmt.executeQuery("SELECT * FROM Floor " +
+					"WHERE FloorID = " + id + "");
+			//TODO: convert result into something, or return relevant strings
+			System.out.println("FloorID " + id + " info " );
+			while(results.next())
+			{
+				String bld = results.getString(2);
+				int lvl = results.getInt(3);
+				System.out.println(bld + " " + lvl);
+			}
+			results.close();
+			stmt.close();
+		}
+		catch (SQLException e){
+			e.printStackTrace();
+		}
+	}
+
 	/*
 	 * insert new provider
 	 */
@@ -413,6 +473,23 @@ public class DatabaseController {
 			stmt = connection.createStatement();
 			stmt.execute("insert into " + officeTable +
 					" values (" + provID + ", " + locID + ")");
+			stmt.close();
+		}
+		catch (SQLException sqlExcept)
+		{
+			sqlExcept.printStackTrace();
+		}
+	}
+
+	/*
+ 	* insert new floor
+ 	*/
+	public static void insertFloor(int floorID, String name, int lvl){
+		try
+		{
+			stmt = connection.createStatement();
+			stmt.execute("insert into " + floorTable +
+					" values (" + floorID + ", '" + name + "', "+ lvl + ")");
 			stmt.close();
 		}
 		catch (SQLException sqlExcept)
@@ -576,6 +653,23 @@ public class DatabaseController {
 	}
 
 	/*
+ 	* delete a floor from the floor table
+ 	 */
+	public static void removeFloorByID(int ID){
+		try
+		{
+			stmt = connection.createStatement();
+			stmt.execute("DELETE FROM " + floorTable +
+					" WHERE FloorID = " + ID + "");
+			stmt.close();
+		}
+		catch (SQLException sqlExcept)
+		{
+			sqlExcept.printStackTrace();
+		}
+	}
+
+	/*
 	 * modify a provider's entry in the table
 	 * TODO: Probably should break this down to modify a single field at a time
 	 * TODO: Fix return type?
@@ -586,8 +680,8 @@ public class DatabaseController {
 			stmt = connection.createStatement();
 			stmt.executeUpdate("UPDATE Provider " +
 					"SET ProviderID = " + ID + ", " +
-					"FirstName = " + fname + ", " +
-					"LastName = " + lname + ", " +
+					"FirstName = '" + fname + "', " +
+					"LastName = '" + lname + "' " +
 					"WHERE ProviderID = " + ID +
 					"");
 			stmt.close();
@@ -608,14 +702,38 @@ public class DatabaseController {
 		try
 		{
 			stmt = connection.createStatement();
-			stmt.executeUpdate("UPDATE Provider " +
+			stmt.executeUpdate("UPDATE Location " +
 					"SET LocationID = " + ID + ", " +
-					"LocationName = " + name + ", " +
-					"LocationType = " + type + ", " +
+					"LocationName = '" + name + "', " +
+					"LocationType = '" + type + "', " +
 					"XCoord = " + x + ", " +
 					"YCoord = " + y + ", " +
-					"FloorID = " + floor + ", " +
+					"FloorID = " + floor + " " +
 					"WHERE LocationID = " + ID +
+					"");
+			stmt.close();
+		} catch (SQLException e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+
+	/*
+ 	* modify a floor's entry in the table
+ 	* TODO: Probably should break this down to modify a single field at a time
+ 	* TODO: Fix return type?
+ 	*/
+	public static void modifyFloor(int ID, String name, int lvl)
+	{
+		try
+		{
+			stmt = connection.createStatement();
+			stmt.executeUpdate("UPDATE Floor " +
+					"SET FloorID = " + ID + ", " +
+					"Building = '" + name + "', " +
+					"FloorLevel = " + lvl + " " +
+					"WHERE FloorID = " + ID +
 					"");
 			stmt.close();
 		} catch (SQLException e)
