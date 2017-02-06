cb51f825b2c7330bdf0fb0b745803143af5f0488
solashirai <solashakashirai@gmail.com>
2017-02-06 11:03:12 -0500

    Reformat code

--- src/main/java/data/Floor.java
+++ src/main/java/data/Floor.java
@@ -3,26 +3,45 @@ package data;
 /*
  * Floor currently does nothing other than exist.
  */
-public class Floor {
+public class Floor
+{
     private int FloorID;
     private String FloorName;
     private int FloorLevel;
 
-    public Floor(){}
+    public Floor()
+    {
+    }
 
-    public Floor(int id, String name, int lvl){
+    public Floor(int id, String name, int lvl)
+    {
         FloorID = id;
         FloorName = name;
         FloorLevel = lvl;
     }
 
-    public int getID(){ return FloorID; }
+    public int getID()
+    {
+        return FloorID;
+    }
 
-    public String getName(){ return FloorName; }
+    public String getName()
+    {
+        return FloorName;
+    }
 
-    public void setName(String n){ FloorName  = n; }
+    public void setName(String n)
+    {
+        FloorName = n;
+    }
 
-    public int getLevel(){ return FloorLevel; }
+    public int getLevel()
+    {
+        return FloorLevel;
+    }
 
-    public void setLevel(int l){ FloorLevel = l; }
+    public void setLevel(int l)
+    {
+        FloorLevel = l;
+    }
 }
