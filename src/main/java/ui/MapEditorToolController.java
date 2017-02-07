7404d88263e093488125dcc8d7be346ce41d555b
solashirai <solashakashirai@gmail.com>
2017-02-06 20:34:16 -0500

    Add mapeditortool controller

---
+++ src/main/java/ui/MapEditorToolController.java
@@ -0,0 +1,15 @@
+package ui;
+
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import data.*;
+import pathfinding.*;
+
+public class MapEditorToolController
+{
+}
