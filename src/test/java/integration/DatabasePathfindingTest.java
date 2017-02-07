a0da2cfc807f505e348367c8585e71d401ee5eaa
Sourec <crmyers@wpi.edu>
2017-02-07 18:50:45 -0500

    Add glitchy test code... yes, this will fail, and it's my fault

--- src/test/java/integration/DatabasePathfindingTest.java
+++ src/test/java/integration/DatabasePathfindingTest.java
@@ -61,4 +61,60 @@ public class DatabasePathfindingTest
 		assertTrue(dNode1.getNeighbors().contains(dNode2));
 		assertTrue(dNode2.getNeighbors().contains(dNode1));
 	}
+
+	@Test
+	public void databaseTortureTest()
+	{
+		//Lets have a little fun with the database by giving it 10000 nodes to manage
+		//Start by initializing and linking 100x100 nodes together
+		System.out.println("Beginning graph creation");
+		ConcreteNode[][] gridNodes = new ConcreteNode[100][100];
+		for (int i = 0; i <100; i++)
+			for (int j = 0; j < 100; j++)
+				gridNodes[i][j] = new ConcreteNode(i * 100 + j, new ArrayList<String>(), i, j, null);
+
+		System.out.println("Finished initializing graph");
+		for (int i = 0; i < 100; i++)
+		{
+			for (int j = 0; j < 100; j++)
+			{
+				if (i > 0)
+					gridNodes[i][j].addNeighbor(gridNodes[i-1][j]);
+				if (i < 99)
+					gridNodes[i][j].addNeighbor(gridNodes[i+1][j]);
+				if (j > 0)
+					gridNodes[i][j].addNeighbor(gridNodes[i][j-1]);
+				if (j < 99)
+					gridNodes[i][j].addNeighbor(gridNodes[i][j+1]);
+			}
+		}
+		System.out.println("Finished constructing graph");
+
+		//Make sure we can find a path between upper left and lower right. If this fails, the below tests will too
+		assertNotNull(graph.findPath(gridNodes[0][0], gridNodes[99][99]));
+		assertEquals(199, graph.findPath(gridNodes[0][0], gridNodes[99][99]).size());
+		System.out.println("Finished base test of graph");
+
+		//Now put them all in the database...
+		for (int i = 0; i < 100; i++)
+			for (int j = 0; j < 100; j++)
+				DatabaseController.insertNode(gridNodes[i][j]);
+		System.out.println("Finished inserting nodes into database");
+
+		//...and get them all back out again!
+		Node[][] dGridNodes = new ConcreteNode[100][100];
+		for (int i = 0; i < 100; i++)
+		{
+			for (int j = 0; j < 100; j++)
+			{
+				dGridNodes[i][j] = DatabaseController.getNodeByID(i * 100 + j);
+				assertNotNull(dGridNodes[i][j]);
+				assertTrue(dGridNodes[i][j].getNeighbors().size() >= 2);
+			}
+		}
+		System.out.println("Finished retrieving nodes from database");
+		//Now try and find the same path found above using these nodes extracted from the database
+		assertNotNull(graph.findPath(dGridNodes[0][0], dGridNodes[99][99]));
+		assertEquals(199, graph.findPath(dGridNodes[0][0], dGridNodes[99][99]).size());
+	}
 }
