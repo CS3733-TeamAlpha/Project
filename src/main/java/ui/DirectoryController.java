e2fc64f4747533b72884e3542ba0adb29a21cd42
Ari Elfenbein <elfenari123@gmail.com>
2017-02-08 19:50:58 -0500

    Delete search bars
    Removed non-functional search bars, to be re-added in a later iteration

--- src/main/java/ui/DirectoryController.java
+++ src/main/java/ui/DirectoryController.java
@@ -1,12 +1,16 @@
 package ui;
 
+import data.DatabaseController;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TextField;
+import pathfinding.Node;
 
+import javax.xml.crypto.Data;
 import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  * Created by Ari on 2/6/17.
@@ -15,16 +19,14 @@ public class DirectoryController
 {
 
 	@FXML
-	private TextField searchField;
-
-	@FXML
 	private TableColumn nameColumn;
 
 	public DirectoryController(){}
 
 	public void initialize()
 	{
-		//TODO - read from database and populate tables
+		ArrayList<Node> nodes = DatabaseController.getAllNodes();
+
 	}
 
 	public void showMap()
