1260aba12cbed391f98233f564602b197326f601
Sourec <crmyers@wpi.edu>
2017-02-11 16:17:58 -0500

    Switch to deleting test database in platform-independent way

--- src/test/java/data/DatabaseTest.java
+++ src/test/java/data/DatabaseTest.java
@@ -4,22 +4,46 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.File;
+
 import static org.junit.Assert.*;
 
 public class DatabaseTest
 {
+	private static final String TEST_DB = "junit_testing_db";
+
 	Database database;
+
 	@Before
 	public void setUp() throws Exception
 	{
-		database = new Database("junit_testing_db");
+		database = new Database(TEST_DB);
 	}
 
 	@After
 	public void tearDown() throws Exception
 	{
 		database.disconnect();
-		java.lang.Runtime.getRuntime().exec("rm -rf junit_testing_db"); //Delete the testing database. TODO: Get this running on windows
+
+		//Delete the test database, stored in a folder $TEST_DB
+		deleteFolder(TEST_DB);
+	}
+
+	/**
+	 * Recursively deletes a folder, because java is stupid and `rm -rf $fname` isn't platform independent.
+	 * @param fname
+	 */
+	private void deleteFolder(String fname)
+	{
+		File folder = new File(fname);
+		for (String s : folder.list())
+		{
+			File curFile = new File(folder.getPath(), s);
+			if (curFile.isDirectory())
+				deleteFolder(curFile.getPath());
+			curFile.delete();
+		}
+		folder.delete();
 	}
 
 	@Test
