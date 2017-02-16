013ec37006f1a82bfd750424dbda4ad82c21211a
blamb135 <bllam@wpi.edu>
2017-02-16 11:25:34 -0500

    Login screen says View Directory instead of Edit Directory

--- src/main/resources/db/DBCreate.sql
+++ src/main/resources/db/DBCreate.sql
@@ -3,7 +3,7 @@ CREATE TABLE Nodes
   node_uuid VARCHAR(36) PRIMARY KEY NOT NULL,
   posX DOUBLE NOT NULL,
   posY DOUBLE NOT NULL,
-  type INTEGER, --can be an enum in java?
+  type INTEGER, --can be an enum in java?  //options are service, provider, hallway
   floor INTEGER NOT NULL,
   building VARCHAR(36) NOT NULL DEFAULT '00000000-0000-0000-0000-000000000000',
   name VARCHAR(128)
