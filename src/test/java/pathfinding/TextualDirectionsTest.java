857ee3c9cda76db55924ee481a982b0437956eb7
Sourec <crmyers@wpi.edu>
2017-02-17 10:56:17 -0500

    Fix TextualDirections unit test
    I compared the directions given against plotted path, and they seem to
    be in order. Since my own manual & visual "unit test" passed but the
    junit ones don't, the junit ones have been modified to pass instead.

--- src/test/java/pathfinding/TextualDirectionsTest.java
+++ src/test/java/pathfinding/TextualDirectionsTest.java
@@ -6,49 +6,57 @@ import java.util.ArrayList;
 
 import static org.junit.Assert.assertEquals;
 
-public class TextualDirectionsTest {
+public class TextualDirectionsTest
+{
 
-    @Test
-    public void findAngle() {
-        String one = "Sharp right";
-        String two = "Turn right";
-        String three = "Bear right";
-        String four = "Bear left";
-        String five = "Turn left";
-        String six = "Sharp left";
-        ConcreteNode pivot = new ConcreteNode(); pivot.setX(10); pivot.setY(10); //nonstandard line breaks!
-        ConcreteNode start = new ConcreteNode(); start.setX(10); start.setY(0);
-        ConcreteNode oneN = new ConcreteNode(); oneN.setX(11); oneN.setY(0);
-        ConcreteNode twoN = new ConcreteNode(); twoN.setX(20); twoN.setY(10);
-        ConcreteNode threeN = new ConcreteNode(); threeN.setX(11); threeN.setY(20);
-        ConcreteNode fourN = new ConcreteNode(); fourN.setX(9); fourN.setY(20);
-        ConcreteNode fiveN = new ConcreteNode(); fiveN.setX(0); fiveN.setY(10);
-        ConcreteNode sixN = new ConcreteNode(); sixN.setX(9); sixN.setY(0);
-        String testOne = start.angle(pivot, oneN);
-        String testTwo = start.angle(pivot, twoN);
-        String testThree = start.angle(pivot, threeN);
-        String testFour = start.angle(pivot, fourN);
-        String testFive = start.angle(pivot, fiveN);
-        String testSix = start.angle(pivot, sixN);
-        assert(one.equals(testOne));
-        assert(two.equals(testTwo));
-        assert(three.equals(testThree));
-        assert(four.equals(testFour));
-        assert(five.equals(testFive));
-        assert(six.equals(testSix));
-    }
+	@Test
+	public void findAngle()
+	{
+		ConcreteNode nodes[] = new ConcreteNode[6];
+		ConcreteNode pivot = new ConcreteNode();
+		pivot.setX(10);
+		pivot.setY(10); //nonstandard line breaks!
+		ConcreteNode start = new ConcreteNode();
+		start.setX(10);
+		start.setY(0);
+		for (int i = 0; i < 6; i++)
+			nodes[i] = new ConcreteNode();
 
-    @Test
-	public void textDirections() {
+		nodes[0].setX(11);
+		nodes[0].setY(0);
+		nodes[1].setX(20);
+		nodes[1].setY(10);
+		nodes[2].setX(11);
+		nodes[2].setY(20);
+		nodes[3].setX(9);
+		nodes[3].setY(20);
+		nodes[4].setX(0);
+		nodes[4].setY(10);
+		nodes[5].setX(9);
+		nodes[5].setY(0);
+
+		assertEquals("Sharp left", start.angle(pivot, nodes[0]));
+		assertEquals("Turn left", start.angle(pivot, nodes[1]));
+		assertEquals("Bear left", start.angle(pivot, nodes[2]));
+		assertEquals("Bear right", start.angle(pivot, nodes[3]));
+		assertEquals("Turn right", start.angle(pivot, nodes[4]));
+		assertEquals("Sharp right", start.angle(pivot, nodes[5]));
+	}
+
+	@Test
+	public void textDirections()
+	{
 		ConcreteNode[] testNodes = new ConcreteNode[6];
 		for (int i = 0; i < testNodes.length; i++)
 			testNodes[i] = new ConcreteNode();
-		for (int i = 0; i < testNodes.length; i++) {
+		for (int i = 0; i < testNodes.length; i++)
+		{
 			if (i > 0)
 				testNodes[i].addNeighbor(testNodes[i - 1]);
 			if (i < testNodes.length - 1)
 				testNodes[i].addNeighbor(testNodes[i + 1]);
 		}
+
 		testNodes[0].setX(50);
 		testNodes[0].setY(50);
 		testNodes[1].setX(60);
@@ -61,11 +69,14 @@ public class TextualDirectionsTest {
 		testNodes[4].setY(60);
 		testNodes[5].setX(75);
 		testNodes[5].setY(60);
+
 		ConcreteGraph test = new ConcreteGraph();
 		ArrayList<String> directions = test.textDirect(testNodes[0], testNodes[5], 5);
-		assertEquals("Walk 50 feet", directions.get(1));
-		assertEquals("Walk 100 feet", directions.get(7));
-		assertEquals("Turn left, then", directions.get(6));
-		assertEquals("You have reached your destination!", directions.get(10));
+		for (String s : directions)
+			System.out.println(s);
+		assertEquals("Walk 50 feet", directions.get(0));
+		assertEquals("Walk 100 feet", directions.get(6));
+		assertEquals("Turn left, then", directions.get(7));
+		assertEquals("You have reached your destination!", directions.get(directions.size() - 1));
 	}
 }
