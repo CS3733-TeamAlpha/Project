a865c7e91c78cd880f9db1a26b774e135db4b76d
Colin Willoughby <colinjw3@gmail.com>
2017-02-21 12:15:51 -0500

    Added searching to the user directory

--- src/main/java/ui/controller/UserDirectoryController.java
+++ src/main/java/ui/controller/UserDirectoryController.java
@@ -3,11 +3,15 @@ package ui.controller;
 import data.Provider;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
+import javafx.collections.transformation.SortedList;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import ui.Paths;
+
+import java.util.ArrayList;
 
 public class UserDirectoryController extends BaseController
 {
@@ -19,22 +23,43 @@ public class UserDirectoryController extends BaseController
 	public TextField searchBar;
 
 	private ObservableList<Provider> observableList;
+	private FilteredList<Provider> filteredData;
+	private ArrayList<Provider> fullList;
 
 	public UserDirectoryController()
 	{
-		observableList = FXCollections.observableArrayList(database.getProviders());
+		fullList = database.getProviders();
+		observableList = FXCollections.observableArrayList(fullList);
 	}
 
 	@Override
 	public void initialize()
 	{
+		filteredData = new FilteredList<>(observableList, p -> true);
+		SortedList<Provider> sortedProviders = new SortedList<Provider>(filteredData);
+		sortedProviders.comparatorProperty().bind(tableView.comparatorProperty());
+
 		firstNameColumn.setCellValueFactory(new PropertyValueFactory<>("firstName"));
 		lastNameColumn.setCellValueFactory(new PropertyValueFactory<>("lastName"));
 		titleColumn.setCellValueFactory(new PropertyValueFactory<>("title"));
 		locationsColumn.setCellValueFactory(new PropertyValueFactory<>("StringLocations"));
 
-		tableView.setItems(observableList);
+		tableView.setItems(sortedProviders);
 		tableView.getSortOrder().add(lastNameColumn);
+
+		searchBar.textProperty().addListener((observable, oldValue, newValue) ->
+		{
+			String query = newValue.toLowerCase().trim();
+			if(query.isEmpty())
+			{
+				filteredData.setPredicate(p -> true);
+			}
+			else
+			{
+				filteredData.setPredicate(provider -> ((provider.getFirstName().toLowerCase() + " " + provider.getLastName().toLowerCase()).contains(query))
+														|| provider.getStringLocations().toLowerCase().contains(query));
+			}
+		});
 	}
 
 	public void backButton()
