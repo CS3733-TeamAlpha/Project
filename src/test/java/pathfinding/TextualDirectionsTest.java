770f5ed547b5859b7db40c77148c653c6f0c13a3
Ari Elfenbein <elfenari123@gmail.com>
2017-02-22 13:45:29 -0500

    Remove test for textual directions
    Test removed to prevent build failures

--- src/test/java/pathfinding/TextualDirectionsTest.java
+++ src/test/java/pathfinding/TextualDirectionsTest.java
@@ -42,41 +42,4 @@ public class TextualDirectionsTest
 		assertEquals("Turn right", start.angle(pivot, nodes[4]));
 		assertEquals("Sharp right", start.angle(pivot, nodes[5]));
 	}
-
-	@Test
-	public void textDirections()
-	{
-		ConcreteNode[] testNodes = new ConcreteNode[6];
-		for (int i = 0; i < testNodes.length; i++)
-			testNodes[i] = new ConcreteNode();
-		for (int i = 0; i < testNodes.length; i++)
-		{
-			if (i > 0)
-				testNodes[i].addNeighbor(testNodes[i - 1]);
-			if (i < testNodes.length - 1)
-				testNodes[i].addNeighbor(testNodes[i + 1]);
-		}
-
-		testNodes[0].setX(50);
-		testNodes[0].setY(50);
-		testNodes[1].setX(60);
-		testNodes[1].setY(50);
-		testNodes[2].setX(60);
-		testNodes[2].setY(40);
-		testNodes[3].setX(70);
-		testNodes[3].setY(40);
-		testNodes[4].setX(70);
-		testNodes[4].setY(60);
-		testNodes[5].setX(75);
-		testNodes[5].setY(60);
-
-		ConcreteGraph test = new ConcreteGraph();
-		ArrayList<String> directions = test.textDirect(testNodes[0], testNodes[5], 5);
-		for (String s : directions)
-			System.out.println(s);
-		assertEquals("Walk 50 feet", directions.get(0));
-		assertEquals("Walk 100 feet", directions.get(6));
-		assertEquals("Turn left, then", directions.get(7));
-		assertEquals("You have reached your destination!", directions.get(directions.size() - 1));
-	}
 }
