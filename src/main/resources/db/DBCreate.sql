0238aaf2e298518b08ea51f032f05effa3854dbe
Sourec <crmyers@wpi.edu>
2017-02-22 23:31:38 -0500

    Make service names unique

--- src/main/resources/db/DBCreate.sql
+++ src/main/resources/db/DBCreate.sql
@@ -21,7 +21,7 @@ CREATE TABLE Edges
 CREATE TABLE Services --Shops, cafes, etc. What is this, a mall?
 (
   node VARCHAR(36),
-  name VARCHAR(128) NOT NULL,
+  name VARCHAR(128) NOT NULL UNIQUE ,
   FOREIGN KEY (node) REFERENCES Nodes(node_uuid) ON DELETE CASCADE --services and nodes have 1-1 relationship
 );
 
