b484ad2150082073f5df850979a566b5ebc15e52
Adam B. White <abwhite@wpi.edu>
2017-02-27 20:25:46 -0500

    Add Emergency Exit Pathfinder

--- src/main/java/pathfinding/EmergencyExitGraph.java
+++ src/main/java/pathfinding/EmergencyExitGraph.java
@@ -3,13 +3,68 @@ package pathfinding;
 import data.Node;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
 
 /**
  * Created by Adam B. White on 2/27/2017.
  */
 public class EmergencyExitGraph extends Graph{
+    /**
+     * Finds path to the nearest Emergency exit.
+     * @param start Node to start pathing from.
+     * @param end   Ignored
+     * @return      Path
+     */
     @Override
     public ArrayList<Node> findPath(Node start, Node end) {
-        return null;
+        System.out.println("Activating breadth first search");
+        ArrayList<Node> result = new ArrayList<Node>();
+        HashMap<Node, Node> parentMap = new HashMap<Node, Node>();
+        parentMap.put(start, null);
+        LinkedList<Node> nodeQueue = new LinkedList<Node>();
+
+        if (start == null)
+            return null;
+
+        if (start == end)
+        {
+            ArrayList<Node> ret = new ArrayList<>();
+            ret.add(start);
+            return ret;
+        }
+
+        for (Node n : start.getNeighbors())
+        {
+            nodeQueue.add(n);
+            parentMap.put(n, start);
+        }
+
+        while (!nodeQueue.isEmpty())
+        {
+            Node temp = nodeQueue.poll();
+            if (temp.getType() >= 6 && temp.getType() <= 19)
+            {
+                while (parentMap.get(temp) != null)
+                {
+                    if (filterNode(start, end, temp))
+                        result.add(temp);
+                    temp = parentMap.get(temp);
+                }
+                result.add(start);
+                return result;
+            } else
+            {
+                for (Node n : temp.getNeighbors())
+                {
+                    if (!parentMap.containsKey(n))
+                    {
+                        nodeQueue.add(n);
+                        parentMap.put(n, temp);
+                    }
+                }
+            }
+        }
+        return null; //no path found
     }
 }
